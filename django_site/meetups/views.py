1ebd1e5d9cba91c9698ee0380ff58caf816a95fd
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-24 14:50:55 +0200

    1:42
    1: 42
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- django_site/meetups/views.py
+++ django_site/meetups/views.py
@@ -21,7 +21,10 @@ def meetup_detail(request, meetup_slug):
         'title': 'runde perler',
         'description': 'sjov med perler'
     }
-    return render(request, 'meetups/meetup-details.html', {
+    return render(request, 'meetups/meetup_details.html', {
         'meetup_title': selected_meetup['title'],
         'meetup_description': selected_meetup['description']
     })
+
+
+    # 1:31 fra https://www.youtube.com/watch?v=t7DrJqcUviA&list=RDCMUCSJbGtTlrDami-tDGPUV9-w&index=6
