28a8319a1df6620acddf66a61f2e0b1b78b35233
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-24 20:54:16 +0200

    2:52
    2: 52
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- django_site/meetups/models.py
+++ django_site/meetups/models.py
@@ -2,11 +2,23 @@ from email.mime import image
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
 class Meetup(models.Model):
     title = models.CharField(max_length=60)
     slug = models.SlugField(unique=True)
     description =models.TextField(max_length=200)
     image = models.ImageField(upload_to='images')
-    #location =models.
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     #address = models.
+
+    def __str__(self):
+        return f'{self.title} - {self.slug}'
+
+
     
