d9a4e0fdd693cc789991ed231dc2019e4fe0d9c8
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-24 22:55:50 +0200

    3:09
    3: 09
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- django_site/meetups/models.py
+++ django_site/meetups/models.py
@@ -19,13 +19,15 @@ class Participant(models.Model):
 
 class Meetup(models.Model):
     title = models.CharField(max_length=60)
+    organizer_email = models.EmailField()
+    date = models.DateField()
     slug = models.SlugField(unique=True)
     description =models.TextField(max_length=200)
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     participants = models.ManyToManyField(Participant, blank=True)
 
-    #address = models.
+    
 
     def __str__(self):
         return f'{self.title} - {self.slug}'
--- django_site/meetups/views.py
+++ django_site/meetups/views.py
@@ -14,8 +14,8 @@ def meetup_detail(request, meetup_slug):
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
         return render(request, 'meetups/meetup_details.html', {
             'meetup_found':True,
-            'meetup_title': selected_meetup.title,
-            'meetup_description': selected_meetup.description,
+            'meetup': selected_meetup
+            
         })
     except Exception as exc:
         return render(request, 'meetups/meetup_details.html', {
