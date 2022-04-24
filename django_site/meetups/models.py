fd34b022823e1f741dda1f6f7640437d7f7a5526
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-24 22:35:21 +0200

    3:02
    3: 02
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- django_site/meetups/models.py
+++ django_site/meetups/models.py
@@ -9,12 +9,22 @@ class Location(models.Model):
     def __str__(self):
         return f'{self.name} ({self.address})'
 
+
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=60)
     slug = models.SlugField(unique=True)
     description =models.TextField(max_length=200)
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant, blank=True)
+
     #address = models.
 
     def __str__(self):
