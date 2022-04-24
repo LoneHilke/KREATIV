1812d63e8c203210140582c8eda720238c46fffd
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-24 15:24:48 +0200

    2:07
    2: 07
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- django_site/meetups/models.py
+++ django_site/meetups/models.py
@@ -1,3 +1,11 @@
+from email.mime import image
 from django.db import models
 
 # Create your models here.
+class Meetup(models.Model):
+    title = models.CharField(max_length=60)
+    slug = models.SlugField(unique=True)
+    description =models.TextField(max_length=200)
+    #location =models.
+    #address = models.
+    #image = models.
