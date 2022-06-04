c046d8464435e60dbab0cae69a55f149200cf483
Your Name <lonehilke@gmail.com>
2022-06-04 23:15:32 +0200

    ???
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -129,3 +129,13 @@ MEDIA_ROOT = BASE_DIR / 'uploads'
 MEDIA_URL = '/files/'
 
 django_heroku.settings(locals())
+import os
+if 'ON_HEROKU' in os.environ:
+    ALLOWED_HOSTS.append('kreativmedperler.herokuapp.com')
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    DEBUG = os.getenv('DEBUG') == 'TRUE'
+    SECRET_KEY = os.getenv('SECRET_KEY')
