8179a193507797d4eb800b1f3ca540e271ae68ab
Lone Hilke <lonehilke@gmail.com>
2022-06-08 21:42:43 +0200

    db
    db
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import django_heroku
 from pathlib import Path
+from environ import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +82,12 @@ WSGI_APPLICATION = 'django_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("NAME"),
+        "USER": env("Kreativ"),
+        "PASSWORD": env("19lone72"), 
+        "HOST": env("DATABASE_HOST"),
+        "PORT": 5432,
     }
 }
 
